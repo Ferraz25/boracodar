53b28925257199de65eef120da8f2f464f1ede18
Ferraz25 <victorferraz244@gmail.com>
2023-10-24 17:54:48 -0300

    add javascript of project 35 - reminder-water

--- 35 - reminder-water/script.js
+++ 35 - reminder-water/script.js
@@ -1,1 +1,13 @@
 lucide.createIcons()
+
+const button = document.querySelector("button")
+const modal = document.querySelector("dialog")
+const buttonClose = document.querySelector("#close")
+
+button.onclick = function () {
+  modal.showModal()
+}
+
+buttonClose.onclick = function () {
+  modal.close()
+}
